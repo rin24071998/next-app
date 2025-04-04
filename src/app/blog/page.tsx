3240574e8078d244c00092987307bc522c93ab81
rinvn.vn <rin.nguyen@hitachivantara.com>
2025-04-04 13:41:47 +0700

    Add tailwind

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata() {
 //   return res.json();
 // }
 export default async function Page() {
-//   const posts = await getData();
+  //   const posts = await getData();
   return (
     <div>
       {/* <ul>
@@ -39,6 +39,35 @@ export default async function Page() {
             width={500}
             height={500}
         /> */}
+        <article>
+          <img
+            className="float-left w-100"
+            src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=1000&q=90"
+          />
+          <img
+            className="float-right w-125"
+            src="https://images.unsplash.com/photo-1434394354979-a235cd36269d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&h=1000&q=90"
+          />
+          <p className="clear-left">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32. The standard chunk of
+            Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+            Malorum" by Cicero are also reproduced in their exact original form,
+            accompanied by English versions from the 1914 translation by H.
+            Rackham.
+          </p>
+        </article>
       </div>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure all JSX/TSX files are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
